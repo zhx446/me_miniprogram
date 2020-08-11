99c71a99c42e292a03ff2ac83f8f26c6e0a8d849
zhx446 <58761665+zhx446@users.noreply.github.com>
2020-08-11 16:10:29 +0800

    8.11  16.10

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -1,60 +1,110 @@
 // pages/mine/mine.js
+const app = getApp();
+// 获取数据库引用
+const db = wx.cloud.database();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        info:['积分','储值金','优惠券'],
-        cell:[
-            {
-                title:'会员等级',
-                icon:'gem-o'
+        loginIs: false,
+        info: ['积分', '储值金', '优惠券'],
+        userInfo: {},
+        cell: [{
+                title: '会员等级',
+                icon: 'gem-o'
             },
             {
-                title:'福利社',
-                icon:'bill-o'
+                title: '福利社',
+                icon: 'bill-o'
             },
             {
-                title:'会员码',
-                icon:'qr'
+                title: '会员码',
+                icon: 'qr'
             },
             {
-                title:'积分商城',
-                icon:'shop-o'
+                title: '积分商城',
+                icon: 'shop-o'
             },
             {
-                title:'我的订单',
-                icon:'orders-o'
+                title: '我的订单',
+                icon: 'orders-o'
             },
             {
-                title:'收货地址',
-                icon:'location-o'
+                title: '收货地址',
+                icon: 'location-o'
             },
             {
-                title:'兑换码',
-                icon:'coupon-o'
+                title: '兑换码',
+                icon: 'coupon-o'
             },
             {
-                title:'集点送',
-                icon:'point-gift-o'
+                title: '集点送',
+                icon: 'point-gift-o'
             },
             {
-                title:'微商城',
-                icon:'bag-o'
+                title: '微商城',
+                icon: 'bag-o'
             },
             {
-                title:'邀请有礼',
-                icon:'friends-o'
+                title: '邀请有礼',
+                icon: 'friends-o'
             }
-            
+
         ]
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.setLogin()
+    },
+    setLogin:function(e) {
+        wx.login({
+            success (res) {
+              if (res.code) {
+                //发起网络请求
+                wx.request({
+                  url: 'GET https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
+                  data: {
+                    code: res.code
+                  }
+                })
+              } else {
+                console.log('登录失败！' + res.errMsg)
+              }
+            }
+          })
+    },
+    userLogin: function (e) {
+        if (e.detail.userInfo) {
+            db.collection('users').add({
+                    // data 字段表示需新增的 JSON 数据
+                    data: {
+                        nickName: e.detail.userInfo.nickName,
+                        avatarUrl: e.detail.userInfo.avatarUrl,
+                        phoneNumber: '',
+                        realName: '',
+                        time: new Date()
+                    }
+                })
+                .then(res => {
+                    db.collection('users').where({
+                        _openid:'o5Bj84tF2bD54h6SNYVoIa7bHt6o'
+                      }).get({
+                        success: function(res) {
+                        // 输出 [{ "title": "The Catcher in the Rye", ... }]
+                        console.log(res)
+                       }
+                      })
+                })
+            this.setData({
+                loginIs: true,
+                userInfo: e.detail.userInfo
+            })
+            app.userInfo = e.detail.userInfo
+        }
     },
 
     /**
